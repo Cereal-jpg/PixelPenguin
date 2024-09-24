8fec6ab1bc02509a909720f247db8ea01cdf9794
Angel Cerdán <angelcerdan1@hotmail.com>
2024-09-24 13:31:34 -0500

    DAO Curricula (No terminado)

---
+++ Proyecto/SoftPixelPenguinsCurriculaDA/src/src/pe/edu/pucp/pixelpenguins/curricula/dao/NotaDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package src.pe.edu.pucp.pixelpenguins.curricula.dao;
+
+import java.util.ArrayList;
+import pe.edu.pucp.pixelpenguins.curricula.Nota;
+
+/**
+ *
+ * @author User
+ */
+public interface NotaDAO {
+    public Integer insertar(Nota seccion);
+    
+    public Integer modificar(Nota seccion);
+    
+    public Integer eliminar(Nota seccion);
+    
+    public ArrayList<Nota> listarTodos();
+    
+    public Nota obtenerPorId(String idNota);
+}

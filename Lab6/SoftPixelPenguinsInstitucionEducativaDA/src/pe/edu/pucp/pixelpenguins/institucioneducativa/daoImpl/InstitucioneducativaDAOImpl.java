a4d5c9553d9e938fd32c560239be1b83a40bc0ec
Alfredo123255 <a20220825@pucp.edu.pe>
2024-09-24 18:28:40 -0500

    InstitucionEducativa

--- Lab6/SoftPixelPenguinsInstitucionEducativaDA/src/pe/edu/pucp/pixelpenguins/institucioneducativa/daoImpl/InstitucioneducativaDAOImpl.java
+++ Lab6/SoftPixelPenguinsInstitucionEducativaDA/src/pe/edu/pucp/pixelpenguins/institucioneducativa/daoImpl/InstitucioneducativaDAOImpl.java
@@ -8,12 +8,39 @@ import java.util.ArrayList;
 
 import pe.edu.pucp.pixelpenguins.institucioneducativa.dao.InstitucioneducativaDAO;
 import pe.edu.pucp.pixelpenguins.institucioneducativa.model.InstitucionEducativa;
-
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import pe.edu.pucp.pixelpenguins.config.DBManager;
 public class InstitucioneducativaDAOImpl implements InstitucioneducativaDAO{
-    
+    private Connection con; 
+    private CallableStatement cs;
+    private ResultSet rs;
     @Override
     public int insertar(InstitucionEducativa institucion) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int resultado =0;
+          try {
+            con = DBManager.getInstance().getConnection();
+            cs = con.prepareCall("{call INSERTAR_INSTITUCION(?,?,?,?,?)}");
+            cs.setInt(1,institucion.getIdInstitucion());
+            cs.setString(2,institucion.getNombre());
+            cs.setInt(3,institucion.getCantidadAlumnos());
+            cs.setString(4,institucion.getDireccion());
+            cs.setInt(5,institucion.getRuc());
+                    
+            resultado = cs.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally{
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return resultado;
     }
 
     @Override

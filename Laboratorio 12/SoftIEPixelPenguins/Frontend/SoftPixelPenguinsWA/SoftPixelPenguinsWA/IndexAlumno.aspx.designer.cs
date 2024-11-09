890e11592b1f432302510dd7b819604e21a2ce6b
Sheshe <angedavid2000@gmail.com>
2024-11-09 16:31:17 -0500

    Revert "Merge branch 'NewPagos'"
    This reverts commit bffbb5208c8bf8667867312df35a6f5306d0377a, reversing
    changes made to 048ed6e9bda9a8e1f13b3e59f3c73f53c6b1f6d8.

--- Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/IndexAlumno.aspx.designer.cs
+++ Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/IndexAlumno.aspx.designer.cs
@@ -13,32 +13,5 @@ namespace SoftPixelPenguinsWA
 
     public partial class IndexAlumno
     {
-
-        /// <summary>
-        /// Control notificationList.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl notificationList;
-
-        /// <summary>
-        /// Control fechaPagoLiteral.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal fechaPagoLiteral;
-
-        /// <summary>
-        /// Control Estado.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal Estado;
     }
 }

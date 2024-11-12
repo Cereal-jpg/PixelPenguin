d8a11c068aebeabdc1dd6eb2c27bf838acdd354e
EnzoIdk <a20220954@pucp.edu.pe>
2024-11-12 18:57:44 -0500

    VerInfoProfesor e intentoInsertarNotas al matricularse

--- Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/Flujo_Alumno/VerInfoProfesorAlumno.aspx.designer.cs
+++ Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/Flujo_Alumno/VerInfoProfesorAlumno.aspx.designer.cs
@@ -24,6 +24,51 @@ namespace SoftPixelPenguinsWA
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl hTitulo;
 
         /// <summary>
+        /// Control txtCodigoProfesor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigoProfesor;
+
+        /// <summary>
+        /// Control txtEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEspecialidad;
+
+        /// <summary>
+        /// Control txtFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFecha;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtSexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSexo;
+
+        /// <summary>
         /// Control btnRegresar.
         /// </summary>
         /// <remarks>

06ac8ee5af4ad10d32641ee5533034628d1a4b6e
Alfredo123255 <a20220825@pucp.edu.pe>
2024-11-19 23:41:12 -0500

    Cambios

--- Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/Flujo_Alumno/ReporteNotasAlumno.aspx.cs
+++ Laboratorio 12/SoftIEPixelPenguins/Frontend/SoftPixelPenguinsWA/SoftPixelPenguinsWA/Flujo_Alumno/ReporteNotasAlumno.aspx.cs
@@ -3,6 +3,8 @@ using iTextSharp.text;
 using SoftPixelPenguinsWA.ServicioWeb;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -17,25 +19,28 @@ namespace SoftPixelPenguinsWA
         CursoXMatriculaWSClient cursoXMatriculaBO = new CursoXMatriculaWSClient();
         AlumnoWSClient alumnoBO = new AlumnoWSClient();
         InstitucionEducativaWSClient institucionBO = new InstitucionEducativaWSClient();
+        NotaWSClient notaBO = new NotaWSClient();
         institucionEducativa institucion = null;
         alumno alumno = null;
         AnioAcademicoWSClient anioBO = new AnioAcademicoWSClient();
-        anioAcademico anio = null;
         int idAlumno;
+        anioAcademico anio = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Verifica si la página actual es el index
             if (Session["idAlumnoLogueado"] != null)
             {
                 idAlumno = (int)Session["idAlumnoLogueado"];
                 institucion = institucionBO.obtenerInstitucionEducativaPorId(1);
                 txtInstitucion.Text = institucion.nombre;
+                txtRUC.Text = institucion.ruc;
                 txtDireccion.Text = institucion.direccion;
                 alumno = alumnoBO.obtenerAlumnoPorId(idAlumno);
                 txtAlumno.Text = alumno.nombreCompleto;
                 deshabilitarComponentes();
             }
-            gvNotas.DataSource = cursoXMatriculaBO.listarNotasFinalesCursosXAlumno(idAlumno);
+            BindingList<cursoXMatricula> notasFinales = new BindingList<cursoXMatricula>(cursoXMatriculaBO.listarNotasFinalesCursosXAlumno(idAlumno));
+            actualizarNotasFinales(notasFinales);
+            gvNotas.DataSource = notasFinales;
             gvNotas.DataBind();
         }
 
@@ -44,13 +49,106 @@ namespace SoftPixelPenguinsWA
             Response.Redirect("NotasParcialesAlumno.aspx");
         }
 
+        private void actualizarNotasFinales(BindingList<cursoXMatricula> notasFinales)
+        {
+            foreach (cursoXMatricula cursoXMatricula in notasFinales)
+            {
+                BindingList<nota> notasBim1 = new BindingList<nota>(notaBO.listarPorAlumnoCursoYBimestre(idAlumno, cursoXMatricula.curso.idCurso, 1));
+                actualizarNotasBimestre(cursoXMatricula, notasBim1, 1);
+                BindingList<nota> notasBim2 = new BindingList<nota>(notaBO.listarPorAlumnoCursoYBimestre(idAlumno, cursoXMatricula.curso.idCurso, 2));
+                actualizarNotasBimestre(cursoXMatricula, notasBim2, 2);
+                BindingList<nota> notasBim3 = new BindingList<nota>(notaBO.listarPorAlumnoCursoYBimestre(idAlumno, cursoXMatricula.curso.idCurso, 3));
+                actualizarNotasBimestre(cursoXMatricula, notasBim3, 3);
+                BindingList<nota> notasBim4 = new BindingList<nota>(notaBO.listarPorAlumnoCursoYBimestre(idAlumno, cursoXMatricula.curso.idCurso, 4));
+                actualizarNotasBimestre(cursoXMatricula, notasBim4, 4);
+                BindingList<string> notasAux = new BindingList<string>();
+                notasAux.Add(cursoXMatricula.notaBimestre1);
+                notasAux.Add(cursoXMatricula.notaBimestre2);
+                notasAux.Add(cursoXMatricula.notaBimestre3);
+                notasAux.Add(cursoXMatricula.notaBimestre4);
+                actualizarNotaFinal(cursoXMatricula, notasAux);
+            }
+        }
+
+        private void actualizarNotasBimestre(cursoXMatricula cursoXMatricula, BindingList<nota> notas, int bimestre)
+        {
+            double suma = 0.0;
+            int cant = notas.Count;
+            foreach (nota nota in notas)
+            {
+                switch (nota.nota1)
+                {
+                    case "AD":
+                        suma += 20;
+                        break;
+                    case "A":
+                        suma += 16;
+                        break;
+                    case "B":
+                        suma += 12;
+                        break;
+                    case "C":
+                        suma += 8;
+                        break;
+                }
+            }
+            string notaProm = obtenerNotaPromedio(suma / cant);
+            switch (bimestre)
+            {
+                case 1:
+                    cursoXMatricula.notaBimestre1 = notaProm;
+                    break;
+                case 2:
+                    cursoXMatricula.notaBimestre2 = notaProm;
+                    break;
+                case 3:
+                    cursoXMatricula.notaBimestre3 = notaProm;
+                    break;
+                case 4:
+                    cursoXMatricula.notaBimestre4 = notaProm;
+                    break;
+            }
+        }
+
+        private string obtenerNotaPromedio(double promedio)
+        {
+            if (promedio >= 17 && promedio <= 20)
+                return "AD";
+            else if (promedio >= 13 && promedio < 17)
+                return "A";
+            else if (promedio >= 9 && promedio < 13)
+                return "B";
+            else if (promedio > 0 && promedio < 9)
+                return "C";
+            else
+                return "-";
+        }
+
+        private void actualizarNotaFinal(cursoXMatricula cursoXMatricula, BindingList<string> notasAux)
+        {
+            double promedioFinal = obtenerPromNotas(notasAux);
+            cursoXMatricula.notaFinal = obtenerNotaPromedio(promedioFinal);
+            //cursoXMatriculaBO.modificarCursoXMatricula(cursoXMatricula);
+        }
+        private double obtenerPromNotas(BindingList<string> notas)
+        {
+            double suma = 0.0;
+            foreach (string nota in notas)
+            {
+                if (nota == "AD") suma += 20;
+                else if (nota == "A") suma += 16;
+                else if (nota == "B") suma += 12;
+                else if (nota == "C") suma += 8;
+            }
+            return suma / 4;
+        }
         private void deshabilitarComponentes()
         {
             txtInstitucion.Enabled = false;
             txtDireccion.Enabled = false;
             txtAlumno.Enabled = false;
+            txtRUC.Enabled = false;
         }
-
         protected void btnDownloadPDF_Click(object sender, EventArgs e)
         {
             Document pdfDoc = new Document();
@@ -104,14 +202,14 @@ namespace SoftPixelPenguinsWA
                 Alignment = Element.ALIGN_CENTER
             };
             title.SpacingBefore = 10f;
-            
+
             // Datos del alumno
             string[] nombreCompleto = alumno.nombreCompleto.Trim().Split(' ');
             string datosEstudiante = "    IIEE: " + institucion.nombre + "\n";
             datosEstudiante += "    Dirección: " + institucion.direccion + "\n";
             datosEstudiante += "    Apellidos y Nombres: " + nombreCompleto[nombreCompleto.Length - 2];
             datosEstudiante += " " + nombreCompleto[nombreCompleto.Length - 1] + " ";
-            for (int i = 0; i < nombreCompleto.Length-2; i++)
+            for (int i = 0; i < nombreCompleto.Length - 2; i++)
             {
                 datosEstudiante += nombreCompleto[i];
             }
@@ -135,7 +233,7 @@ namespace SoftPixelPenguinsWA
             Font tableFont = FontFactory.GetFont("Arial", 11);
             float tableHeight = 15f;
             float headerHeight = 20f;
-            float[] columnWidths = { 2f, 1f, 1f, 1f, 1f, 1f};
+            float[] columnWidths = { 2f, 1f, 1f, 1f, 1f, 1f };
             pdfTable.SetWidths(columnWidths);
 
             int columnIndex = 0;
@@ -155,7 +253,7 @@ namespace SoftPixelPenguinsWA
                 columnIndex = 0;
                 foreach (TableCell tableCell in gridViewRow.Cells)
                 {
-                    PdfPCell pdfCell = new PdfPCell(new Phrase(tableCell.Text,tableFont));
+                    PdfPCell pdfCell = new PdfPCell(new Phrase(tableCell.Text, tableFont));
                     if (columnIndex == 0)
                     {
                         pdfCell.HorizontalAlignment = Element.ALIGN_LEFT; // Alinear a la derecha
@@ -190,7 +288,7 @@ namespace SoftPixelPenguinsWA
             dataEmitida.SpacingBefore = 40f;
 
             // Colocarlo al último 
-            for(int i = 0; i < 17; i++)
+            for (int i = 0; i < 15; i++)
             {
                 pdfDoc.Add(new Paragraph(" "));
             }
@@ -206,6 +304,5 @@ namespace SoftPixelPenguinsWA
             Response.BinaryWrite(bytes);
             Response.End();
         }
-
     }
 }
